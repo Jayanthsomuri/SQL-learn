130d3978c5abc2a8485b4ac2f397b220aa5e5baf
Jayanth somuri <69779313+Jayanthsomuri@users.noreply.github.com>
2022-02-08 11:45:08 +0530

    Add files via upload

---
+++ Exp-1.sql
@@ -0,0 +1,12 @@
+-- Ex-1
+
+USE sem6;
+CREATE TABLE Student(
+name varchar(10),
+regno int(10),
+dept  varchar(15),
+Batch  varchar(10)
+);
+
+SELECT * FROM student;
+-- creating another table from existing table
